d9a53db01c32e2e0646ce30bd93321a79e211d60
Daniel Helmig <Helmig@fzi.de>
2024-08-18 21:02:26 +0200

    old state

---
+++ scoreboard/src/app/clock.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { useEffect , useState} from "react";
+export default function Clock() {
+    const [hours, setHours] = useState("");
+    const [minutes, setMinutes] = useState("");
+    const [seconds, setSeconds] = useState("");
+
+    
+
+    useEffect(() => {
+        setInterval(() => {
+            const date = new Date();
+            const h = date.getHours().toString().padStart(2, '0');
+            const m = date.getMinutes().toString().padStart(2, '0');
+            const s = date.getSeconds().toString().padStart(2, '0');
+            setHours(h);
+            setMinutes(m);
+            setSeconds(s);    
+        }), 1000;
+    })
+    return (
+        <p>
+            {hours}:{minutes}:{seconds}   
+        </p>
+    )
+}
--- scoreboard/src/app/page.tsx
+++ scoreboard/src/app/page.tsx
@@ -1,13 +1,39 @@
-import {AppBar, Button, Toolbar, Typography} from "@mui/material";
+import {AppBar, Button, Container, Toolbar, Typography} from "@mui/material";
+import Stack from '@mui/material/Stack';
+
+
+import Clock from "./clock";
+import Table from "./table";
 
 export default function Home() {
   return (
+    <>
       <AppBar position="static" elevation={0}>
         <Toolbar>
+          <Stack 
+            direction="row" 
+            justifyContent="space-between" 
+            alignItems="center" 
+            sx={{ width: 1 }}
+          >
             <Typography variant="h6">
-                Scoreboard
+              Turniername hier
             </Typography>
+            <Typography variant="h6">
+              <Clock/>
+            </Typography>
+          </Stack>
         </Toolbar>
       </AppBar>
+      <Container 
+        maxWidth={false}
+        sx={{ 
+          border: 1,
+          borderRadius: 2
+        }}
+      >
+          <Table/>
+      </Container> 
+    </>
   );
 }
