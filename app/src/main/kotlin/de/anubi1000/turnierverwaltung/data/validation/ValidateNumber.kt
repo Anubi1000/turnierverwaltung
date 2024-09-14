59ca2e3a24662c4ef71709704c0365321c352193
Anubi1000 <72503115+Anubi1000@users.noreply.github.com>
2024-09-14 18:01:09 +0200

    add proper double value handling

--- app/src/main/kotlin/de/anubi1000/turnierverwaltung/data/validation/ValidateNumber.kt
+++ app/src/main/kotlin/de/anubi1000/turnierverwaltung/data/validation/ValidateNumber.kt
@@ -4,3 +4,9 @@ fun validateInt(string: String): Int? {
     val int = string.toIntOrNull() ?: return null
     return if (int >= 0) int else null
 }
+
+fun validateDouble(string: String): Double? {
+    val replacedString = string.replace(',', '.')
+    val double = replacedString.toDoubleOrNull()
+    return double
+}
