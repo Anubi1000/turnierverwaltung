6a3e16743afb4fc285d0a1e5f2ec454269b5a7b7
Anubi1000 <72503115+Anubi1000@users.noreply.github.com>
2024-09-19 19:20:52 +0200

    Extract temp realm

--- app/src/test/kotlin/de/anubi1000/turnierverwaltung/data/repository/TournamentRepositoryImplTest.kt
+++ app/src/test/kotlin/de/anubi1000/turnierverwaltung/data/repository/TournamentRepositoryImplTest.kt
@@ -1,46 +1,32 @@
 package de.anubi1000.turnierverwaltung.data.repository
 
-import de.anubi1000.turnierverwaltung.database.createBaseRealmConfig
 import de.anubi1000.turnierverwaltung.database.model.Tournament
 import de.anubi1000.turnierverwaltung.database.queryById
+import de.anubi1000.turnierverwaltung.util.tempRealm
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.engine.spec.tempdir
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldStartWith
-import io.realm.kotlin.Realm
 import kotlinx.coroutines.flow.first
 import java.time.Duration
 import java.time.Instant
 
 @Suppress("ktlint:standard:class-signature")
 class TournamentRepositoryImplTest : FunSpec({
-    val dir = tempdir()
-
-    lateinit var realm: Realm
+    val realm = tempRealm()
     lateinit var repository: TournamentRepositoryImpl
 
     beforeTest {
-        realm = Realm.open(
-            createBaseRealmConfig()
-                .directory(dir.path)
-                .inMemory()
-                .build(),
-        )
-        repository = TournamentRepositoryImpl(realm)
-    }
-
-    afterTest {
-        realm.close()
-        // Run gc so realm file is closed otherwise the deletion of the temp dir fails
-        System.gc()
+        repository = TournamentRepositoryImpl(realm())
     }
 
     context("getAllAsFlow") {
         test("sorts correctly") {
+            val realm = realm()
             val tournaments = mutableListOf<Tournament>()
+
             for (x in 1..3) {
                 for (y in 1..3) {
                     tournaments.add(
@@ -73,6 +59,7 @@ class TournamentRepositoryImplTest : FunSpec({
 
     context("update") {
         test("updates a tournament") {
+            val realm = realm()
             val tournament = Tournament(
                 name = "Test 1",
                 date = Instant.EPOCH,
