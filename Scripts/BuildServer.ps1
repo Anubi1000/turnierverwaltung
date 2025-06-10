e2190e9538d2d151cc773fabe8b6e35ec8090788
Anubi1000 <72503115+Anubi1000@users.noreply.github.com>
2025-06-10 21:41:56 +0200

    Use publish profile

--- Scripts/BuildServer.ps1
+++
@@ -1,6 +0,0 @@
-﻿$startDir = Get-Location
-Set-Location (Join-Path -Path $PSScriptRoot -ChildPath "../Server")
-
-dotnet publish --no-restore -c Release -r win-x64 --sc true /p:PublishSingleFile=true /p:PublishTrimmed=true
-
-Set-Location $startDir
