08cd00cc19d0e3089265a1ecf05efd9fc46a6395
Anubi1000 <72503115+Anubi1000@users.noreply.github.com>
2025-06-10 20:54:22 +0200

    Update RunServerTests.ps1

--- Scripts/RunServerTests.ps1
+++ Scripts/RunServerTests.ps1
@@ -1,6 +1,6 @@
 ﻿$startDir = Get-Location
 Set-Location (Join-Path -Path $PSScriptRoot -ChildPath "../Server.Tests")
 
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput="./coverage.xml" /p:ExcludeByFile='\"**/Migrations/*.cs,**/Server/obj/**/*.cs,**/Results/Word/*.cs\"'
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput="./coverage.xml" /p:ExcludeByFile='"**/Migrations/*.cs,**/Server/obj/**/*.cs,**/Results/Word/*.cs"'
 
 Set-Location $startDir
